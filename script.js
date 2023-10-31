a19f2b9272918dbfd5cbbf217054383f7794fc6f
yugesharma <mynameisyugesh@gmail.com>
2023-10-31 23:27:54 +0530

    First commit-adding map

---
+++ script.js
@@ -0,0 +1,4 @@
+const map = L.map('map').setView([34, -72], 6);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
