c7c261092d54008ec89b1ba9cf528b07e8fa8bf3
yugesharma <mynameisyugesh@gmail.com>
2023-11-03 21:30:48 +0530

    Converting co-ords format and layout adjusted

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 const oapiKey = '5431cea4928259758e577c8cd26f641d';
 
+//API call for getting real time wind data
+
 function fetchWindData(lat, lon) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${oapiKey}`;
     return fetch(apiUrl)
@@ -14,6 +16,18 @@ function fetchWindData(lat, lon) {
       });
   }
 
+//convert co-ordinates from decimal to degrees and minutes
+
+  function toDMS(lat,lng) {
+    const toDMS=coord=>{min=~~(minA=((a=Math.abs(coord))-(deg=~~a))*60);
+    return deg+"° "+min+"' "+Math.ceil((minA-min)*60)+'"';
+    };
+    var cord= ` ${toDMS(lat)} ${lat>=0?"N":"S"} / ${toDMS(lng)} ${lng>=0?"E":"W"}`;
+    return cord;
+    }
+
+//Capture user click postion and display data obtained from API
+
   map.on('click', function (e) {
     const lat = e.latlng.lat;
     const lng = e.latlng.lng;
@@ -21,7 +35,7 @@ function fetchWindData(lat, lon) {
     fetchWindData(lat, lng).then(data => {
       const windSpeed = Math.round(1.944*data.wind.speed*100)/100;
       const windDirection = data.wind.deg;
-      document.getElementById("data").innerHTML=(`location ${lat} ${lng} Wind speed ${windSpeed} Wind direction ${windDirection}`);
+      document.getElementById("data").innerHTML=(`location ${toDMS(lat,lng)} Wind speed ${windSpeed} Wind direction ${windDirection}`);
      });
   
     L.marker([lat, lng], {pane: 'label'}).addTo(drawnItems);
