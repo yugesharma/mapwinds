de42124992d17797ada80202b59051b198cfc36b
yugesharma <mynameisyugesh@gmail.com>
2023-11-07 21:41:19 +0530

    reading NectCDF4 file

--- grib.py
+++ grib.py
@@ -1,11 +1,38 @@
 import xarray as xr
-import numpy as np
-import json
-import netCDF4 as nc
 
-# Load NetCDF data
-filePath='output.nc'
+data = xr.open_dataset('era5.nc')
 
-dSet=nc.Dataset(filePath)
-for dimension in dSet.dimensions.values():  
-    print(dimension) 
+wind_speed = data['wind_speed']  
+wind_direction = data['wind_direction']  
+
+
+features = []
+
+
+for i in range(len(lats)):
+    for j in range(len(lons)):
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [lons[j], lats[i]]
+            },
+            "properties": {
+                "wind_speed": wind_speed[i],  
+                "wind_direction": wind_direction[i] 
+            }
+        }
+        features.append(feature)
+
+
+geojson_data = {
+    "type": "FeatureCollection",
+    "features": features
+}
+
+
+geojson_str = json.dumps(geojson_data, indent=2)
+
+
+with open('output.geojson', 'w') as geojson_file:
+    geojson_file.write(geojson_str)
