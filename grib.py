b005614826b0b35f87f355c716ed460f87e7643b
yugesharma <mynameisyugesh@gmail.com>
2023-11-08 19:06:55 +0530

    Interpreting NetCDF4 file

--- grib.py
+++ grib.py
@@ -6,9 +6,6 @@ wind_speed = data['wind_speed']
 wind_direction = data['wind_direction']  
 
 
-features = []
-
-
 for i in range(len(lats)):
     for j in range(len(lons)):
         feature = {
@@ -19,20 +16,22 @@ for i in range(len(lats)):
             },
             "properties": {
                 "wind_speed": wind_speed[i],  
-                "wind_direction": wind_direction[i] 
+                "wind_direction": wind_direction[i, j] 
             }
         }
         features.append(feature)
 
-
-geojson_data = {
-    "type": "FeatureCollection",
-    "features": features
-}
-
-
-geojson_str = json.dumps(geojson_data, indent=2)
-
-
-with open('output.geojson', 'w') as geojson_file:
-    geojson_file.write(geojson_str)
+for i in range(len(lats)):
+    for j in range(len(lons)):
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [lons[j], lats[i]]
+            },
+            "properties": {
+                "wind_speed": wind_speed[i, j],  
+                "wind_direction": wind_direction[i, j] 
+            }
+        }
+        features.append(feature)
