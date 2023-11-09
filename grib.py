11afeb631f9ae4a1eaddb2a60d84752c900c842d
yugesharma <mynameisyugesh@gmail.com>
2023-11-09 21:21:32 +0530

    calculating wind data and saving as geojson

--- grib.py
+++ grib.py
@@ -1,37 +1,46 @@
 import xarray as xr
+import numpy as np
+import json
 
-data = xr.open_dataset('era5.nc')
+data = xr.open_dataset('your_file.nc')
 
-wind_speed = data['wind_speed']  
-wind_direction = data['wind_direction']  
+u = data['u']  
+v = data['v']  
 
+lats = u['latitude'].values
+lons = u['longitude'].values
 
-for i in range(len(lats)):
-    for j in range(len(lons)):
-        feature = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [lons[j], lats[i]]
-            },
-            "properties": {
-                "wind_speed": wind_speed[i],  
-                "wind_direction": wind_direction[i, j] 
+features = []
+
+for t in range(u.shape[0]):
+    for i in range(u.shape[1]):
+        for j in range(u.shape[2]):
+            wind_direction_rad = np.arctan2(-u[t, i, j], -v[t, i, j])
+
+            wind_direction_deg = np.degrees(wind_direction_rad)
+
+            wind_direction_deg = (wind_direction_deg + 360) % 360
+
+            feature = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [lons[j], lats[i]]
+                },
+                "properties": {
+                    "wind_speed": u[t, i, j],  
+                    "wind_direction": wind_direction_deg
+                }
             }
-        }
-        features.append(feature)
+            
+            features.append(feature)
 
-for i in range(len(lats)):
-    for j in range(len(lons)):
-        feature = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [lons[j], lats[i]]
-            },
-            "properties": {
-                "wind_speed": wind_speed[i, j],  
-                "wind_direction": wind_direction[i, j] 
-            }
-        }
-        features.append(feature)
+geojson_data = {
+    "type": "FeatureCollection",
+    "features": features
+}
+
+geojson_str = json.dumps(geojson_data, indent=2)
+
+with open('output.geojson', 'w') as geojson_file:
+    geojson_file.write(geojson_str)
