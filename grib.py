d409172151079461a0115dd11e3edbbb0d1ffe8f
yugesharma <mynameisyugesh@gmail.com>
2023-11-17 23:50:44 +0530

    using dask diagnostics to identify bottlenecks

--- grib.py
+++ grib.py
@@ -1,73 +1,84 @@
 import xarray as xr
 import numpy as np
 import json
-from math import atan2, degrees
 import concurrent.futures
-import dask.array as da
 import time
+import dask
+import dask.distributed
+import dask.diagnostics
+import multiprocessing
 start=time.time()
 
 
-class CustomJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.float32):
-            return round(float(obj),4)
-        return super(CustomJSONEncoder, self).default(obj)
-    
-def calculate_wind_direction(args):
-    t, i, j, u, v = args
-    wind_direction_rad = np.arctan2(-u[t, i, j], -v[t, i, j])
-    wind_direction_deg = np.degrees(wind_direction_rad)
-    wind_direction_deg = (wind_direction_deg + 360) % 360
-    wind_speed_formatted = round(float(u[t, i, j]), 4)
-    wind_direction_formatted = round(float(wind_direction_deg), 4)
-    return {
-        "type": "Feature",
-        "geometry": {
-            "type": "Point",
-            "coordinates": [lons[j], lats[i]]
-        },
+client = dask.distributed.Client()
+with dask.diagnostics.profile(filename="dask-profile.html"):
+    print('Start')
 
-        "properties": {
-            "wind_speed": wind_speed_formatted,
-            "wind_direction": wind_direction_formatted
+    class CustomJSONEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, np.float32):
+                return round(float(obj),4)
+            return super(CustomJSONEncoder, self).default(obj)
+        
+    def calculate_wind_direction(args):
+        t, i, j, u, v = args
+        wind_direction_rad = np.arctan2(-u[t, i, j], -v[t, i, j])
+        wind_direction_deg = np.degrees(wind_direction_rad)
+        wind_direction_deg = (wind_direction_deg + 360) % 360
+        wind_speed_formatted = round(float(u[t, i, j]), 4)
+        wind_direction_formatted = round(float(wind_direction_deg), 4)
+        return {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [lons[j], lats[i]]
+            },
+
+            "properties": {
+                "wind_speed": wind_speed_formatted,
+                "wind_direction": wind_direction_formatted
+            }
         }
-    }
 
-data = xr.open_dataset('era5.nc')
-u = data['u10'].chunk(chunks={'time': 1, 'latitude': 121, 'longitude': 481})
-v = data['v10'].chunk(chunks={'time': 1, 'latitude': 121, 'longitude': 481})
-lats = u['latitude'].values
-lons = u['longitude'].values
+    if __name__ == '__main__':
 
-u_da = da.from_array(u, chunks=u.shape)
-v_da = da.from_array(v, chunks=v.shape)
+        multiprocessing.freeze_support()
+        data = xr.open_dataset('era5.nc')
 
-# Extract data variables
-wind_u = data['u10']
-wind_v = data['v10']
-lats = wind_u['latitude'].values
-lons = wind_v['longitude'].values
-u_da = da.from_array(wind_u, chunks=wind_u.shape)
-v_da = da.from_array(wind_v, chunks=wind_v.shape)
-features=[]
+        u = data['u10']
+        v = data['v10']
+        lats = u['latitude'].values
+        lons = u['longitude'].values
 
-args_list = [(t, i, j, wind_u, wind_v) for t in range(wind_u.shape[0])
-                                    for i in range(wind_v.shape[1])
-                                    for j in range(wind_u.shape[2])]
-    
-with concurrent.futures.ThreadPoolExecutor() as executor:  
-    features = list(executor.map(calculate_wind_direction, args_list))
+        chunk_size_time = 2
+        chunk_size_lat = 121 
+        chunk_size_lon = 481  
 
-geojson_data = {
-    "type": "FeatureCollection",
-    "features": features
-}
+        features = []
 
-geojson_str = json.dumps(geojson_data, indent=2, cls=CustomJSONEncoder)
+        for t in range(0, u.shape[0], chunk_size_time):
+            for i in range(0, u.shape[1], chunk_size_lat):
+                for j in range(0, u.shape[2], chunk_size_lon):
+                    u_chunk = u[t:t+chunk_size_time, i:i+chunk_size_lat, j:j+chunk_size_lon]
+                    v_chunk = v[t:t+chunk_size_time, i:i+chunk_size_lat, j:j+chunk_size_lon]
+                    args_list = [(t, i, j, u_chunk, v_chunk, lats, lons)
+                                for t in range(u_chunk.shape[0])
+                                for i in range(u_chunk.shape[1])
+                                for j in range(u_chunk.shape[2])]
+        features=[]
+            
+        with concurrent.futures.ThreadPoolExecutor() as executor:  
+            features.extend(executor.map(calculate_wind_direction, args_list))
 
-with open('output.geojson', 'w') as geojson_file:
-    geojson_file.write(geojson_str)
+        geojson_data = {
+            "type": "FeatureCollection",
+            "features": features
+        }
 
-end=time.time()
-print(end-start)
+        geojson_str = json.dumps(geojson_data, indent=2, cls=CustomJSONEncoder)
+
+        with open('output.geojson', 'w') as geojson_file:
+            geojson_file.write(geojson_str)
+
+        end=time.time()
+        print(end-start)
